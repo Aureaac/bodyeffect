a46f22b1dd60d6a62f525a986576ef303ce5ea5f
Aureaac <programador.analista12@ciudadmaderas.com>
2022-11-24 16:56:02 -0600

    Prueba

--- application/controllers/Agenda.php
+++ application/controllers/Agenda.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+/* Prueba */
 class Agenda extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
